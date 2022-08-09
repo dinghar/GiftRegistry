3e8899baa8d9b4d4c8b2cc9fa3f43dca1006a98f
Ross <rnh8wb@virginia.edu>
2022-08-08 20:09:14 -0700

    More

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { WishList } from "./components/wishList/WishList";
 export default function App() {
   return (
     <>
+      <div id="modal-anchor" />
       <AppScreen>
         <WishList />
       </AppScreen>
--- components/addWishListItem/AddWishListItem.tsx
+++ components/addWishListItem/AddWishListItem.tsx
@@ -1,10 +1,24 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Modal, View } from "react-native";
 import { styles } from "./styles";
+import { useState } from "react";
 
 export function AddWishListItem() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <View style={styles.buttonContainer}>
-      <Button title="Add Item" />
+      <Button title="Add Item" onPress={openModal} />
+      <Modal visible={modalIsOpen} onRequestClose={closeModal}>
+        <Button title="Add" />
+      </Modal>
     </View>
   );
 }
